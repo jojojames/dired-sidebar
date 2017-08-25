9fddbfe743dd7ca0526de25c2c39a1e95c01fc4a
James Nguyen <james@jojojames.com>
2017-08-24 19:23:57 -0700

    Update doc

--- dired-sidebar.el
+++ dired-sidebar.el
@@ -35,10 +35,12 @@
 ;;   (setq dired-subtree-use-backgrounds nil))
 ;;
 ;; (use-package dired-sidebar
+;;   :bind (("C-x C-n" . dired-sidebar/toggle-sidebar))
 ;;   :ensure nil
 ;;   :commands (dired-sidebar/toggle-sidebar)
 ;;   :config
 ;;   (use-package all-the-icons-dired
+;;     ;; M-x all-the-icons-install-fonts
 ;;     :ensure t
 ;;     :commands (all-the-icons-dired-mode)))
 ;;
