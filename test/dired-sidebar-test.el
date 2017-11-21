a0945d825582967afe3c240f0d84cfd2413cf4bf
James Nguyen <james@jojojames.com>
2017-11-20 22:32:45 -0800

    Set up tests

--- test/dired-sidebar-test.el
+++ test/dired-sidebar-test.el
@@ -1,3 +1,8 @@
 ;;; dired-sidebar-test.el --- Tests for dired-sidebar
+(require 'dired-sidebar)
+
+(ert-deftest dired-sidebar-blank-test ()
+  "Blank test."
+  (should (equal 0 0)))
 
 ;;; dired-sidebar-test.el ends here
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,22 @@
-;;; test-helper.el --- Helpers for dired-sidebar-test.el
+;;; test-helper.el --- Helpers for dired-sidebar-test.el -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 James Nguyen
+
+;; Author: James Nguyen <james@jojojames.com>
+;; Maintainer: James Nguyen <james@jojojames.com>
+;; Created: 20 November 2017
+
+;;; Commentary:
+
+;; Utilities for running dired-sidebar tests.
+
+;;; Code:
+(require 'ert)
+
+;; FIXME: Adding `f' as a dependency just for this line.
+(require 'f)
+(let ((smart-jump-dir (f-parent (f-dirname (f-this-file)))))
+  (add-to-list 'load-path smart-jump-dir))
+(require 'dired-sidebar)
 
 ;;; test-helper.el ends here
