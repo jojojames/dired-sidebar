4657964873bab31ee0645e82f89d1ef9e75c3bb5
James Nguyen <james@jojojames.com>
2018-08-15 18:37:59 -0700

    Simplify

--- dired-sidebar.el
+++ dired-sidebar.el
@@ -27,17 +27,10 @@
 ;; This package provides a tree browser similar to `neotree' or `treemacs'
 ;; but leverages `dired' to do the job of display.
 
-;;
 ;; (use-package dired-sidebar
 ;;   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
 ;;   :ensure nil
-;;   :commands (dired-sidebar-toggle-sidebar)
-;;   :config
-;;   (use-package all-the-icons-dired
-;;     ;; M-x all-the-icons-install-fonts
-;;     :ensure t
-;;     :commands (all-the-icons-dired-mode)))
-;;
+;;   :commands (dired-sidebar-toggle-sidebar))
 
 ;;; Code:
 
