1852a0b17bf2619607f6b4dfc437e279ba04e93c
James Nguyen <james@jojojames.com>
2026-06-18 20:56:49 -0400

    Tag

--- dired-sidebar.el
+++ dired-sidebar.el
@@ -5,7 +5,7 @@
 ;; Author: James Nguyen <james@jojojames.com>
 ;; Maintainer: James Nguyen <james@jojojames.com>
 ;; URL: https://github.com/jojojames/dired-sidebar
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "29.1") (dired-subtree "0.0.1") (compat "30.0.0.0"))
 ;; Keywords: dired, files, tools
 ;; HomePage: https://github.com/jojojames/dired-sidebar
